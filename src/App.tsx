87021a7fba6e773008ff5243297896d9ab473c0a
wainer-oliveira <54476162+wainer-oliveira@users.noreply.github.com>
2022-11-16 19:17:31 -0300

    fix: retirado comentario.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,7 @@ import AppRoutes from "./routes"
 import "./App.css"
 
 function App() {
-/*  
-  localStorage.setItem(import.meta.env.VITE_REACT_APP_LOCALSTORAGE_PATIENTS_LIST, JSON.stringify([{
-    nome: "Wainer",
-    dataNascimento: "05/11/1997",
-    cpf: "44534993889",
-    sexo: "masculino",
-    endereco: "Ricardo Pelizaro, 41, Cristais Paulista - SP"
-  }]))
-*/
+
   return (
     <AppRoutes/>
   )
